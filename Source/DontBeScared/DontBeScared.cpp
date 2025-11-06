7591139616b3c8a639985b65d108ba05fc3b2fe5
komarh <hue.komar@mvmntgames.com>
2025-11-06 11:30:59 -0600

    Project Files

---
+++ Source/DontBeScared/DontBeScared.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DontBeScared.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DontBeScared, "DontBeScared" );
